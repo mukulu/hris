e0f24cb4c305f8f59f4294dec750fcba492d8215
John Francis Mukulu <john.f.mukulu@gmail.com>
2013-07-11 05:41:25 +0300

    Implemented prototype menu

---
+++ src/Hris/UserBundle/Menu/MenuBuilder.php
@@ -0,0 +1,54 @@
+<?php
+/*
+ *
+ * Copyright 2012 Human Resource Information System
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ *
+ * @since 2012
+ * @author John Francis Mukulu <john.f.mukulu@gmail.com>
+ *
+ */
+
+namespace Hris\UserBundle\Menu;
+
+use Knp\Menu\MenuItemInterface;
+use Knp\Menu\FactoryInterface;
+use Symfony\Component\HttpFoundation\Request;
+
+class MenuBuilder
+{
+    private $factory;
+
+    /**
+     * @param FactoryInterface $factory
+     */
+    public function __construct(FactoryInterface $factory)
+    {
+        $this->factory = $factory;
+    }
+
+    public function createMainMenu(Request $request)
+    {
+        $menu = $this->factory->createItem('root');
+
+        $menu->addChild('Users', array('route' => 'admin_hris_user_user_list'))
+             ->addChild('InputTypes', array('route' => 'admin_hris_form_inputtype_list'));
+        // ... add more children
+
+        return $menu;
+    }
+}
