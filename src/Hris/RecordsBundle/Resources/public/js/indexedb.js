c4379bcef6fbb60b49724995e4d874936492166c
Yusuph Kulindwa <yusuphk@gmail.com>
2013-08-08 13:31:26 +0300

    adding icon and confifuring hte conf file for the icons

--- src/Hris/RecordsBundle/Resources/public/js/indexedb.js
+++ src/Hris/RecordsBundle/Resources/public/js/indexedb.js
@@ -6,7 +6,7 @@
  * To change this template use File | Settings | File Templates.
  */
 var localDatabase = {};
-//var dbName = "employeeDb";
+var dbName = "employeeDb";
 localDatabase.indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB;
 localDatabase.IDBKeyRange = window.IDBKeyRange || window.webkitIDBKeyRange;
 localDatabase.IDBTransaction = window.IDBTransaction || window.webkitIDBTransaction;
@@ -15,8 +15,8 @@ localDatabase.indexedDB.onerror = function (e) {
     console.log("Database error: " + e.target.errorCode);
 };
 
-function openDatabase(dbName) {
-    var openRequest = localDatabase.indexedDB.open(dbName);
+function openDatabase(databaseName) {
+    var openRequest = localDatabase.indexedDB.open(databaseName);
     openRequest.onerror = function(e) {
         console.log("Database error: " + e.target.errorCode);
     };
@@ -64,6 +64,43 @@ function createDatabase(databaseName, tableName, columnNames) {
         };
 
     };
+}
+
+function addRecords(tableName, dataValues) {
+    console.log("populating database");
+
+    var tx = localDatabase.db.transaction("hris_form", "readwrite");
+    var store = tx.objectStore("hris_form");
+
+    if (localDatabase != null && localDatabase.db != null) {
+        /*
+        Start of store function to send data to database
+         */
+        console.log("populating database with unknown data for testing");
+        var request = store.put({
+            "id": "E1",
+            "first_name" : "Joe",
+            "last_name" : "Smith",
+            "email" : "joe.smith@somedomain.com",
+            "street" : "123 Main Street",
+            "city" : "New York",
+            "state" : "New York",
+            "zip_code" : "10016",
+        });
+        request.onsuccess = function(e) {
+            console.log("Added E1");
+        };
+
+        request.onerror = function(e) {
+            console.log(e.value);
+        };
+
+        tx.oncomplete = function() {
+            console.log("All data is entered into the system");
+        };
+        // end of transactions
+
+    }
 }
 
 function populateDatabase() {
@@ -161,298 +198,5 @@ function populateDatabase() {
         request.onerror = function(e) {
             console.log(e.value);
         };
-    }
-}
-
-function addRecords(tableName, dataValues) {
-    /*
-    Parsing the object to Json format for further use.
-     */
-    tableName = JSON.parse(tableName);
-    dataValues = JSON.parse(dataValues);
-
-
-    for(var i = 0; i < dataValues.length; i++)
-    {
-        console.log( dataValues[i] );
-
-        Object.getOwnPropertyNames(dataValues[i]).forEach(function(val, idx, array) {
-            console.log('"' + val + '":"' + dataValues[i][val] + '"');
-        });
-    }
-
-    try {
-        alert(tableName);
-
-        var result = document.getElementById("result");
-        result.innerHTML = "";
-
-
-        var transaction = localDatabase.db.transaction(tableName, "readwrite");
-        var store = transaction.objectStore(tableName);
-
-        if (localDatabase != null && localDatabase.db != null) {
-            var request = store.add({
-                "id": "E5",
-                "first_name" : "Jane",
-                "last_name" : "Doh",
-                "email" : "jane.doh@somedomain.com",
-                "street" : "123 Pennsylvania Avenue",
-                "city" : "Washington D.C.",
-                "state" : "DC",
-                "zip_code" : "20500",
-            });
-            request.onsuccess = function(e) {
-                result.innerHTML = "Employee record was added successfully.";;
-            };
-
-            request.onerror = function(e) {
-                console.log(e.value);
-                result.innerHTML = "Employee record was not added.";
-            };
-        }
-    }
-    catch(e){
-        console.log(e);
-    }
-}
-
-function updateEmployee() {
-    try {
-        var result = document.getElementById("result");
-        result.innerHTML = "";
-
-        var transaction = localDatabase.db.transaction("employees", "readwrite");
-        var store = transaction.objectStore("employees");
-        var jsonStr;
-        var employee;
-
-        if (localDatabase != null && localDatabase.db != null) {
-
-            store.get("E3").onsuccess = function(event) {
-                employee = event.target.result;
-                // save old value
-                jsonStr = "OLD: " + JSON.stringify(employee);
-                result.innerHTML = jsonStr;
-
-                // update record
-                employee.email = "john.adams@anotherdomain.com";
-
-                var request = store.put(employee);
-
-                request.onsuccess = function(e) {
-                };
-
-                request.onerror = function(e) {
-                    console.log(e.value);
-                };
-
-                // fetch record again
-                store.get("E3").onsuccess = function(event) {
-                    employee = event.target.result;
-                    jsonStr = "<br/>NEW: " + JSON.stringify(employee);
-                    result.innerHTML = result.innerHTML  + jsonStr;
-                };
-            }; // fetch employee first time
-        }
-    }
-    catch(e){
-        console.log(e);
-    }
-}
-
-function clearAllEmployees() {
-    try {
-        var result = document.getElementById("result");
-        result.innerHTML = "";
-
-        if (localDatabase != null && localDatabase.db != null) {
-            var store = localDatabase.db.transaction("employees", "readwrite").objectStore("employees");
-
-            store.clear().onsuccess = function(event) {
-                result.innerHTML = "'Employees' object store cleared";
-            };
-        }
-    }
-    catch(e){
-        console.log(e);
-    }
-}
-
-function fetchEmployee() {
-    try {
-        var result = document.getElementById("result");
-        result.innerHTML = "";
-        if (localDatabase != null && localDatabase.db != null) {
-            var store = localDatabase.db.transaction("employees").objectStore("employees");
-            store.get("E3").onsuccess = function(event) {
-                var employee = event.target.result;
-                if (employee == null) {
-                    result.innerHTML = "employee not found";
-                }
-                else {
-                    var jsonStr = JSON.stringify(employee);
-                    result.innerHTML = jsonStr;
-                }
-            };
-        }
-    }
-    catch(e){
-        console.log(e);
-    }
-}
-
-function fetchAllEmployees() {
-    try {
-        var result = document.getElementById("result");
-        result.innerHTML = "";
-
-        if (localDatabase != null && localDatabase.db != null) {
-            var store = localDatabase.db.transaction("employees").objectStore("employees");
-            var request = store.openCursor();
-            request.onsuccess = function(evt) {
-                var cursor = evt.target.result;
-                if (cursor) {
-                    var employee = cursor.value;
-                    var jsonStr = JSON.stringify(employee);
-                    result.innerHTML = result.innerHTML + "<br/>" + jsonStr;
-                    cursor.continue();
-                }
-            };
-        }
-    }
-    catch(e){
-        console.log(e);
-    }
-}
-
-function fetchNewYorkEmployees() {
-    try {
-        var result = document.getElementById("result");
-        result.innerHTML = "";
-
-        if (localDatabase != null && localDatabase.db != null) {
-            var range = IDBKeyRange.only("New York");
-
-            var store = localDatabase.db.transaction("employees").objectStore("employees");
-            var index = store.index("stateIndex");
-
-            index.openCursor(range).onsuccess = function(evt) {
-                var cursor = evt.target.result;
-                if (cursor) {
-                    var employee = cursor.value;
-                    var jsonStr = JSON.stringify(employee);
-                    result.innerHTML = result.innerHTML + "<br/>" + jsonStr;
-                    cursor.continue();
-                }
-            };
-        }
-    }
-    catch(e){
-        console.log(e);
-    }
-}
-
-function fetchEmployeeByEmail() {
-    try {
-        var result = document.getElementById("result");
-        result.innerHTML = "";
-
-        if (localDatabase != null && localDatabase.db != null) {
-            var range = IDBKeyRange.only("john.adams@somedomain.com");
-
-            var store = localDatabase.db.transaction("employees").objectStore("employees");
-            var index = store.index("emailIndex");
-
-            index.get(range).onsuccess = function(evt) {
-                var employee = evt.target.result;
-                var jsonStr = JSON.stringify(employee);
-                result.innerHTML = jsonStr;
-            };
-        }
-    }
-    catch(e){
-        console.log(e);
-    }
-}
-
-function fetchEmployeeByZipCode1() {
-    try {
-        var result = document.getElementById("result");
-        result.innerHTML = "";
-
-        if (localDatabase != null && localDatabase.db != null) {
-            var store = localDatabase.db.transaction("employees").objectStore("employees");
-            var index = store.index("zipIndex");
-
-            var range = IDBKeyRange.lowerBound("92000");
-
-            index.openCursor(range).onsuccess = function(evt) {
-                var cursor = evt.target.result;
-                if (cursor) {
-                    var employee = cursor.value;
-                    var jsonStr = JSON.stringify(employee);
-                    result.innerHTML = result.innerHTML + "<br/>" + jsonStr;
-                    cursor.continue();
-                }
-            };
-        }
-    }
-    catch(e){
-        console.log(e);
-    }
-}
-
-function fetchEmployeeByZipCode2() {
-    try {
-        var result = document.getElementById("result");
-        result.innerHTML = "";
-
-        if (localDatabase != null && localDatabase.db != null) {
-            var store = localDatabase.db.transaction("employees").objectStore("employees");
-            var index = store.index("zipIndex");
-
-            var range = IDBKeyRange.upperBound("93000");
-
-            index.openCursor(range).onsuccess = function(evt) {
-                var cursor = evt.target.result;
-                if (cursor) {
-                    var employee = cursor.value;
-                    var jsonStr = JSON.stringify(employee);
-                    result.innerHTML = result.innerHTML + "<br/>" + jsonStr;
-                    cursor.continue();
-                }
-            };
-        }
-    }
-    catch(e){
-        console.log(e);
-    }
-}
-
-function fetchEmployeeByZipCode3() {
-    try {
-        var result = document.getElementById("result");
-        result.innerHTML = "";
-
-        if (localDatabase != null && localDatabase.db != null) {
-            var store = localDatabase.db.transaction("employees").objectStore("employees");
-            var index = store.index("zipIndex");
-
-            var range = IDBKeyRange.bound("92000", "92999", true, true);
-
-            index.openCursor(range).onsuccess = function(evt) {
-                var cursor = evt.target.result;
-                if (cursor) {
-                    var employee = cursor.value;
-                    var jsonStr = JSON.stringify(employee);
-                    result.innerHTML = result.innerHTML + "<br/>" + jsonStr;
-                    cursor.continue();
-                }
-            };
-        }
-    }
-    catch(e){
-        console.log(e);
     }
 }
