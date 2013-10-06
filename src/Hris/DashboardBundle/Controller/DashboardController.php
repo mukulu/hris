73f30e43eaf31963840aa6d5edb185585ed14df9
Wilfred Senyoni <senyoni@gmail.com>
2013-10-06 22:33:53 +0300

    Dashboard charts

--- src/Hris/DashboardBundle/Controller/DashboardController.php
+++ src/Hris/DashboardBundle/Controller/DashboardController.php
@@ -24,12 +24,15 @@
  */
 namespace Hris\DashboardBundle\Controller;
 
+use Hris\FormBundle\Entity\Field;
+use Hris\ReportsBundle\Controller\ReportAggregationController;
 use JMS\SecurityExtraBundle\Annotation\Secure;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Ob\HighchartsBundle\Highcharts\Highchart;
+use Doctrine\Common\Collections\ArrayCollection;
 use Zend\Json\Expr;
 
 /**
@@ -53,19 +56,81 @@ class DashboardController extends Controller
      */
     public function indexAction()
     {
+        //get the detail for the active user
+        $user = $this->container->get('security.context')->getToken()->getUser();
+        $reportAggregationController =  New ReportAggregationController();
+        $entityManager = $this->getDoctrine()->getManager();
+        $organisationunitGroups = new ArrayCollection();
+        $fieldTwo = $entityManager->getRepository('HrisFormBundle:Field')->findOneByName
+            (array('name'=> "ContactsofNextofKin"));
+
+
+
+        $forms = new ArrayCollection();
+        $formObjects = $entityManager->getRepository('HrisFormBundle:Form')->findAll();
+        foreach($formObjects as $formObject){
+            $forms->add($formObject) ;
+        }
+
+        //pull the fields needed
+        $dashboardEmplyomentField = $entityManager->getRepository('HrisFormBundle:Field')->findOneByName
+                (array('name'=> "EmploymentDistribution"));
+
+        $dashboardRetirementField = $entityManager->getRepository('HrisFormBundle:Field')->findOneByName
+            (array('name'=> "RetirementDistribution"));
+
+        $dashboardAgeField = $entityManager->getRepository('HrisFormBundle:Field')->findOneByName
+            (array('name'=> "AgeDistribution"));
+
+        $dashboardGenderField = $entityManager->getRepository('HrisFormBundle:Field')->findOneByName
+            (array('name'=> "Sex"));
+
+        //get the data from the aggregation engine
+        $dashboardEmplyomentChart = $reportAggregationController::aggregationEngine($user->getOrganisationunit(), $forms,$dashboardEmplyomentField, $organisationunitGroups, TRUE, $dashboardEmplyomentField);
+
+        $dashboardRetirementChart = $reportAggregationController::aggregationEngine($user->getOrganisationunit(), $forms,$dashboardRetirementField, $organisationunitGroups, TRUE, $dashboardRetirementField);
+
+        $dashboardAgeChart = $reportAggregationController::aggregationEngine($user->getOrganisationunit(), $forms,$dashboardAgeField, $organisationunitGroups, TRUE, $dashboardAgeField);
+
+        $dashboardGenderChart = $reportAggregationController::aggregationEngine($user->getOrganisationunit(), $forms, $dashboardGenderField, $organisationunitGroups, TRUE, $dashboardGenderField);
+
+        /*
+         * Prepare the combinationdashboardchart Chart
+         */
+        $categories = range(date('Y')-10, date('Y'));
+        $emplyomentData = array();
+        $retirementData = array();
+        foreach($categories as $category){
+            $returnValue = $this->arraySearchAction(strval($category),$dashboardEmplyomentChart);
+            if( $returnValue != false){
+                $emplyomentData[] = $dashboardEmplyomentChart[$returnValue]['total'];
+            }else{
+                $emplyomentData[] = 0;
+            }
+
+            $returnValue = $this->arraySearchAction(strval($category),$dashboardRetirementChart);
+            if( $returnValue != false){
+                $retirementData[] = $dashboardRetirementChart[$returnValue]['total'];
+            }else{
+                $retirementData[] = 0;
+            }
+
+        }
+        //get the values data
         $series = array(
             array(
                 'name'  => 'Employment',
                 'type'  => 'column',
                 'color' => '#0D0DC1',
-                'yAxis' => 1,
-                'data'  => array(1952,1058,1805,2082,2964,4243,3251,4979,2991,3171,426),
+                'yAxis' => 0,
+                'data'  => $emplyomentData,
             ),
             array(
                 'name'  => 'Retirement',
                 'type'  => 'spline',
                 'color' => '#AA4643',
-                'data'  => array(994,1503,1119,1380,1289,1840,1633,2048,1496,2045,1836),
+                'yAxis' => 1,
+                'data'  => $retirementData,
                 'dashStyle'=>'longdash',
             ),
             array(
@@ -78,12 +143,12 @@ class DashboardController extends Controller
                 'data'  => array(
                     array(
                         'name'=>'Female',
-                        'y'=> 41848,
+                        'y'=> $dashboardGenderChart[0]['total'],
                         'color'=>'#66ECA0'
                     ),
                     array(
                         'name'=>'Male',
-                        'y'=>20315,
+                        'y'=>$dashboardGenderChart[1]['total'],
                         'color'=>'#9494d4'
                     ),
                 ),
@@ -113,16 +178,16 @@ class DashboardController extends Controller
                 ),
             ),
         );
-        $categories = array('2003', '2004', '2005', '2005', '2006', '2007', '2008', '2009', '2010', '2011', '2012', '2013');
 
-        $dashboardchart = new Highchart();
-        $dashboardchart->chart->renderTo('chart_placeholder'); // The #id of the div where to render the chart
-        $dashboardchart->chart->type('column');
-        $dashboardchart->title->text('Employment Distribution');
-        $dashboardchart->subtitle->text('Ministry of Health And Social Welfare with lower levels');
-        $dashboardchart->xAxis->categories($categories);
-        $dashboardchart->yAxis($yData);
-        $dashboardchart->legend->enabled(false);
+
+        $combinationdashboardchart = new Highchart();
+        $combinationdashboardchart->chart->renderTo('chart_placeholder'); // The #id of the div where to render the chart
+        $combinationdashboardchart->chart->type('column');
+        $combinationdashboardchart->title->text('Employment Distribution, Retirement Distribution with Gender Report');
+        $combinationdashboardchart->subtitle->text($user->getOrganisationunit()->getLongname().' with lower levels');
+        $combinationdashboardchart->xAxis->categories($categories);
+        $combinationdashboardchart->yAxis($yData);
+        $combinationdashboardchart->legend->enabled(false);
         $formatter = new Expr('function () {
                  var unit = {
                      "Retirement": "retirements",
@@ -135,8 +200,33 @@ class DashboardController extends Controller
                     return this.x + ": <b>" + this.y + "</b> " + unit;
                  }
              }');
-        $dashboardchart->tooltip->formatter($formatter);
-        $dashboardchart->series($series);
+        $combinationdashboardchart->tooltip->formatter($formatter);
+        $combinationdashboardchart->series($series);
+
+        /*
+         * Prepare the Retirement Chart
+         */
+        $categories = range(date('Y'), date('Y')+10);
+        $retirementData = array();
+        foreach($categories as $category){
+            $returnValue = $this->arraySearchAction(strval($category),$dashboardRetirementChart);
+            if( $returnValue != false){
+                $retirementData[] = $dashboardRetirementChart[$returnValue]['total'];
+            }else{
+                $retirementData[] = 0;
+            }
+        }
+
+        $retirementChart = $this->constructChartAction($dashboardRetirementField,$retirementData,$user->getOrganisationunit(),$categories,'column','Retirement Distribution','retirementdistribution');
+
+        /*
+        * Prepare the Age Distribution Chart
+        */
+        foreach($dashboardAgeChart as $dashboardAgeCharts){
+            $categories[] = $dashboardAgeCharts[strtolower($dashboardAgeField->getName())];
+            $data[] =  $dashboardAgeCharts['total'];
+        }
+        $ageChart = $this->constructChartAction($dashboardAgeField,$data,$user->getOrganisationunit(),$categories,'column','Age Distribution','agedistribution');
 
         /*
          * Messaging
@@ -147,8 +237,100 @@ class DashboardController extends Controller
         if(empty($unreadMessages)) $unreadMessages= 0;
 
         return array(
-            'chart'=>$dashboardchart,
+            'combinationchart'=>$combinationdashboardchart,
+            'retirementchart'=>$retirementChart,
+            'agechart'=>$ageChart,
             'unreadmessages'=>$unreadMessages
         );
+    }
+    /**
+     * Finds and displays a dashboards for the user.
+     *
+     * @Route("/dashboard", name="dashboard_show")
+     * @Method("GET")
+     * @Template()
+     */
+    public function showAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+        exit();
+    }
+
+    /**search the multidimension array for a needle
+     * @param $needle
+     * @param $haystack
+     * @return bool|int|string
+     */
+    private function arraySearchAction($needle,$haystack) {
+        foreach($haystack as $key=>$value) {
+            $current_key=$key;
+            if($needle===$value OR (is_array($value) && $this->arraySearchAction($needle,$value) !== false)) {
+                return $current_key;
+            }
+        }
+        return false;
+    }
+
+    private  function constructChartAction($field,$data,$organisationUnit,$categories, $graph, $title,$placeholder){
+
+
+        $series = array(
+            array(
+                'name'  => $field->getName(),
+                'data'  => $data,
+            ),
+        );
+        $formatterLabel = $field->getCaption();
+
+        $yData = array(
+            array(
+                'labels' => array(
+                    'formatter' => new Expr('function () { return this.value + "" }'),
+                    'style'     => array('color' => '#0D0DC1')
+                ),
+                'title' => array(
+                    'text'  => $field->getCaption(),
+                    'style' => array('color' => '#0D0DC1')
+                ),
+                'opposite' => true,
+            ),
+            array(
+                'labels' => array(
+                    'formatter' => new Expr('function () { return this.value + "" }'),
+                    'style'     => array('color' => '#AA4643')
+                ),
+                'gridLineWidth' => 1,
+                'title' => array(
+                    'text'  => $field->getCaption(),
+                    'style' => array('color' => '#AA4643')
+                ),
+            ),
+        );
+
+        $dashboardchart = new Highchart();
+        $dashboardchart->chart->renderTo($placeholder); // The #id of the div where to render the chart
+        $dashboardchart->chart->type($graph);
+        $dashboardchart->title->text($title);
+        $dashboardchart->subtitle->text($organisationUnit->getLongname().' with lower levels');
+        $dashboardchart->xAxis->categories($categories);
+        $dashboardchart->yAxis($yData);
+        if($field->getId() == $field->getId())$dashboardchart->legend->enabled(true); else $dashboardchart->legend->enabled(true);
+
+        $formatter = new Expr('function () {
+                 var unit = {
+
+                     "'.$formatterLabel.'" : "'. strtolower($formatterLabel).'",
+
+                 }[this.series.name];
+                 if(this.point.name) {
+                    return ""+this.point.name+": <b>"+ this.y+"</b> "+ this.series.name;
+                 }else {
+                    return this.x + ": <b>" + this.y + "</b> " + this.series.name;
+                 }
+             }');
+        $dashboardchart->tooltip->formatter($formatter);
+        if($graph == 'pie')$dashboardchart->plotOptions->pie(array('allowPointSelect'=> true,'dataLabels'=> array ('format'=> '<b>{point.name}</b>: {point.percentage:.1f} %')));
+        $dashboardchart->series($series);
+        return $dashboardchart;
     }
 }
