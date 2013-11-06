3296559cfc4b29391427a3dead71249d3830bce5
John Francis Mukulu <john.f.mukulu@gmail.com>
2013-11-06 04:32:20 +0300

    Added dhis intergration to main menu

--- src/Hris/IntergrationBundle/Event/ConfigureMenuEvent.php
+++ src/Hris/IntergrationBundle/Event/ConfigureMenuEvent.php
@@ -31,7 +31,7 @@ use Symfony\Component\EventDispatcher\Event;
 
 class ConfigureMenuEvent extends Event
 {
-    const CONFIGURE = 'hris_integration.menu_configure';
+    const CONFIGURE = 'hris_intergration.menu_configure';
 
     private $factory;
     private $menu;
