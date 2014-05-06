a09e776aa8933c412afa3af9f282fd39af42439a
Yusuph Kulindwa <yusuphk@gmail.com>
2014-05-06 22:29:32 +0300

    re-arrangeing the combo box to be in alphabetical order

--- src/Hris/RecordsBundle/Resources/public/js/indexedb.js
+++ src/Hris/RecordsBundle/Resources/public/js/indexedb.js
@@ -312,6 +312,20 @@ function loadFieldOptions(fieldUIDS, databaseName) {
                 }
                 else {
                     console.log('No more Matching Fields Options');
+
+                    $("select").each(function () {
+
+                        // Keep track of the selected option.
+                        var selectedValue = $(this).val();
+
+                        // Sort all the options by text. I could easily sort these by val.
+                        $(this).html($("option", $(this)).sort(function (a, b) {
+                            return a.text == b.text ? 0 : a.text < b.text ? -1 : 1
+                        }));
+
+                        // Select one option.
+                        $(this).val(selectedValue);
+                    });
                 }
 
             }
